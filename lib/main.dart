52a82655640b8c0efea6737341b70811ddf5006a
Bobonovagulnoza <bobonovagulnoza68@gmail.com>
2024-12-13 15:56:27 +0500

    uy ishi togirlandi

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,75 @@
 import "package:flutter/material.dart";
 
-
 void main() {
-  print("...");
-
+  runApp(MyApp());
 }
+
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+
+      home: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            "Bobonova Gulnoza",
+            style: TextStyle(
+              color: Colors.indigoAccent,
+              fontWeight: FontWeight.bold,
+              fontSize: 32,
+              fontFamily: "Roboto",
+              decoration: TextDecoration.none,
+            ),
+          ),
+          Text(
+            "Navoiy viloyati",
+            style: TextStyle(
+              color: Colors.indigoAccent,
+              fontWeight: FontWeight.bold,
+              fontSize: 32,
+              fontFamily: "Roboto",
+              decoration: TextDecoration.none,
+            ),
+          ),
+          Text(
+            "2003 yil 17 aprel",
+            style: TextStyle(
+              color: Colors.indigoAccent,
+              fontWeight: FontWeight.bold,
+              fontSize: 32,
+              fontFamily: "Roboto",
+              decoration: TextDecoration.none,
+            ),
+          ),
+          Text(
+            "TATU  talabasi",
+            style: TextStyle(
+              color: Colors.indigoAccent,
+              fontWeight: FontWeight.bold,
+              fontSize: 32,
+              fontFamily: "Roboto",
+              decoration: TextDecoration.none,
+            ),
+          ),
+          Text(
+            "Flutter mobil dasturchisi",
+            style: TextStyle(
+              color: Colors.indigoAccent,
+              fontWeight: FontWeight.bold,
+              fontSize: 32,
+              fontFamily: "Roboto",
+              decoration: TextDecoration.none,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
